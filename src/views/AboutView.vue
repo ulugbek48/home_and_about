c88381fddbdfaac5b06f1ecebaa746f0a3d08ea7
ulugbek48 <ulugbekyuldashboyev@gmail.com>
2023-09-21 12:27:39 -0400

    pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,245 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="container">
+
+        <div class="right">
+            <div class="right_box">
+                <div class="right_title">
+                    <p class="title_p">STAY IN CAONTACT</p>
+                    <h1 class="title_h">Get in Touch</h1>
+                </div>
+                <div class="right_boxes">
+                    <div class="inp">
+                        <input class="name" v-model="info.name" type="text" name="" id="" placeholder="Name" required>
+                        <input class="phone" type="text" name="" id="" placeholder="Phone No." v-model="info.mobile" required>
+                    </div>
+                    <input class="email" type="email" name="" id="" placeholder="Email I'd" v-model="info.email" required>
+                    <input class="proj" type="text" name="" id="" placeholder="Project Brief..." v-model="info.message" required>
+                    <div class="inp_2">
+                        <input class="chbox" type="checkbox" name="" id="">
+                        <label class="label" for="">Subscribe Newslatter</label>
+                    </div>
+                    <button class="btn_box" type="submit"><span  class="span">Send Message</span></button>
+                </div>
+            </div>
+        </div>
+
+        <div class="left">
+            <img src="../assets/phone_email_sms.png" alt="">
+        </div>
+    </div>
 </template>
+
+<script setup>
+
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+const val = ref(null);
+const info = ref({
+  name: "",
+  mobile: "",
+  email: "",
+  message: "",
+});
+const route = useRouter();
+
+const check = (e) => {
+  e.preventDefault();
+
+  route.push({ name: "cars" });
+};
+</script>
+
+<style lang="scss" scoped>
+
+.container{
+    width: 1920px;
+    display: inline-flex;
+    flex-direction: row;
+    height: 1045px;
+    padding-left: 0px;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 138px;
+}
+
+.right{
+    width: 986px;
+    height: 1045px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.right_box {
+    width: 712px;
+    height: 797px;
+    display: flex;
+    padding: 75px;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.right_title{
+    width: 303px;
+    height: 98px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 13px;
+    margin-right: 100px;
+}
+
+.title_p{
+    color: #000;
+    text-align: center;
+
+    /* Body03 */
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 120%; /* 19.2px */
+}
+
+.title_h{
+    color: #000;
+    text-align: center;
+
+    /* Heading02 */
+    font-family: Encode Sans;
+    font-size: 55px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 120%; /* 66px */
+}
+
+.right_boxes{
+    width: 562px;
+    display: flex;
+    height: 508px;
+    flex-direction: column;
+    align-items: center;
+    gap: 47px;
+    align-self: stretch;
+}
+
+.inp{
+    width: 576px;
+    height: 63px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 25px;
+    align-self: stretch;
+    margin-right: 10px;
+}
+
+.name{
+    width: 271px;
+    display: flex;
+    height: 63.396px;
+    padding: 10px 5px;
+    align-items: center;
+    gap: 10px;
+    flex: 1 0 0;
+}
+
+.phone{
+    width: 271px;
+    display: flex;
+    height: 63.396px;
+    padding: 10px 5px;
+    align-items: center;
+    gap: 10px;
+    flex: 1 0 0;
+}
+
+.email{
+    width: 562px;
+    display: flex;
+    height: 63.396px;
+    padding: 10px 5px;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+}
+
+.proj{
+    width: 562px;
+    display: flex;
+    height: 63px;
+    padding: 10px 5px;
+    align-items: center;
+    gap: 10px;
+    align-self: stretch;
+}
+
+
+.inp_2{
+    width: 562px;
+    height: 40px;
+    display: flex;
+    padding: 10px;
+    align-items: flex-start;
+    gap: 10px;
+    align-self: stretch;
+}
+
+.label{
+    width: 139px;
+    height: 17px;
+    color: var(--foundation-dark-normal, #5C5C5C);
+    text-align: center;
+
+    /* Paragraphbold02 */
+    font-family: Encode Sans;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 120%; /* 16.8px */
+}
+
+.chbox{
+    width: 20px;
+    height: 20px; 
+    border: 1px solid #FFF;
+    background: var(--foundation-light-normal-active, #EEECE7);
+    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.10);   
+}
+
+.btn_box{
+    width: 562px;
+    display: flex;
+    height: 75.849px;
+    padding: 10px 23px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+    align-self: stretch;
+    border-radius: 8px;
+    border: 0.5px solid #C4C4C4;
+    background: var(--foundation-dark-normal, #5C5C5C);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.04);
+}
+
+.span{
+    width: 140px;
+    height: 24px;
+    color: #FFF;
+    text-align: center;
+
+    /* Body02 */
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 120%; /* 24px */
+}
+.left img{
+    width: 934px;
+    height: 1045px;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,324 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="total">
+
+        
+        <div class="main1">
+            <div class="main2">
+                <div class="icon-main">
+                    <div class="iconic1">
+                        <img src="../assets/call.png" alt="">              
+                    </div>
+                    <div class="main-title">
+                        <h1 class="h1">Give Us a Call</h1>
+                        <p class="p1"> +91 098 098 8393</p>
+
+                    </div>
+                </div>
+                <div class="icon-main">
+                    <div class="iconic1">
+                        <img src="../assets/email.png" alt="">              
+                    </div>
+                    <div class="main-title">
+                        <h1 class="h1">Send Us an Email</h1>
+                        <p class="p1">ecample@gmail.com</p>
+
+                    </div>
+                </div>
+
+                <div class="icon-main">
+                    <div class="iconic1">
+                        <img src="../assets/location.png" alt="">              
+                    </div>
+                    <div class="main-title">
+                        <h1 class="h1">Drop BY and Talk</h1>
+                        <p class="p1">Unit 8012- 8014, Chandaka Industrial Estate, K I, I T University, Patia, Bhubaneswar, Odisha 751024</p>
+
+                    </div>
+                </div>
+
+
+                <div class="insta">
+                    <img src="../assets/twitter.png" alt="">
+                    <img src="../assets/in.png" alt="">
+                    <img src="../assets/insta.png" alt="">
+                    <img src="../assets/facebook.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="main_right">
+          <div class="container">
+            <div class="container_title">
+              <p class="title_p">STAY IN CAONTACT</p>
+              <h1 class="title_h">Get in Touch</h1>
+            </div>
+            <div class="container_box">
+              <form action="#">
+                <input class="box_inp" type="text" name="" v-model="info.name" id="" placeholder="Name">
+                <input class="box_inp" type="text" name="" v-model="info.mobile" id="" placeholder="Mobile">
+                <input class="box_inp" type="email" name="" v-model="info.email" id="" placeholder="Email">
+                <textarea type="text" v-model="info.message" class="box_text" placeholder="Message"></textarea>
+                <button class="box_btn" @click="check"><h2 class="h2">SUBMIT</h2></button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from "vue-router";
+const val = ref(null);
+const info = ref({
+  name: "",
+  mobile: "",
+  email: "",
+  message: "",
+});
+const route = useRouter();
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+const check = () => {
+  console.log(info.value.name);
+  if (info.value.name != "Ulugbek") {
+    alert("Wrong name");
+    return;
+  }
+  if (info.value.mobile != "+998917772597") {
+    alert("Wrong mobile number");
+    return;
+  }
+  if (info.value.email != "ulugbekyuldashboyev@gmail.com") {
+    alert("Wrong email address");
+    return;
+  }
+
+  route.push({ name: "about" });
+};
+</script>
+
+<style lang="scss" scoped>
+.total {
+  width: 1920px;
+  height: 1045px;
+  display: flex;
+  flex-direction: row;
+}
+.main1 {
+  width: 845px;
+  height: 1045px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 259px 54px 260px 120px;
+}
+
+.main2 {
+  width: 671px;
+  display: inline-flex;
+  height: 526px;
+  padding: 10px;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+}
+
+
+.icon-main {
+    width: 651px;
+    height: 90px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 36px;
+    align-self: stretch;
+    
+}
+.iconic1 {
+  width: 90px;
+  height: 90px;
+  display: flex;
+  padding: 30px;
+  align-items: flex-start;
+  gap: 10px;
+}
+
+.main-title{
+    width: 525px;
+    height: 90px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 21px;
+    flex: 1 0 0;
+}
+
+.h1{
+    width: 218px;
+    height: 42px;
+    color: var(--foundation-painted-pony-normal-hover, #A78768);
+
+/* Headingbold04 */
+font-family: Encode Sans;
+font-size: 35px;
+font-style: normal;
+font-weight: 600;
+line-height: 120%; /* 42px */
+}
+
+
+.p1{
+    width: 100%;
+    color: var(--foundation-dark-dark-active, #0A0A0A);
+font-family: Poppins;
+font-size: 18px;
+font-style: normal;
+font-weight: 300;
+line-height: normal;
+text-transform: capitalize;
+
+}
+.insta{
+    width: 651px;
+    background-color: red($color: #000000);
+    display: flex;
+padding: 0px 1px;
+align-items: center;
+gap: 22px;
+align-self: stretch;
+margin-left: 20px;
+
+}
+
+.insta img{
+    display: flex;
+    flex-direction:row;
+    align-items: flex-start;
+
+    gap: 10px;
+}
+
+.main_right {
+  width: 1075px;
+  height: 1045px;
+  flex-shrink: 0;
+  background: var(--foundation-painted-pony-normal, #B99674);
+  padding: 99px 162px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.container{
+  display: flex;
+  width: 751px;
+  height: 847px;
+  padding: 35px 45px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 64px;
+  flex-shrink: 0;
+  border-radius: 15px;
+  background: var(--foundation-light-white, #FFF);
+}
+
+.container_title{
+  width: 303px;
+  height: 99px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+}
+
+.title_p{
+  color: var(--foundation-painted-pony-darker, #413529);
+  text-align: center;
+
+  /* Paragraph01 */
+  font-family: Encode Sans;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 25px; /* 138.889% */
+}
+
+.title_h{
+  color: var(--foundation-painted-pony-darker, #413529);
+  text-align: center;
+
+  /* Heading02 */
+  font-family: Encode Sans;
+  font-size: 55px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%; /* 66px */
+}
+
+.container_box{
+  width: 684px;
+  display: flex;
+  height: 426px;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-shrink: 0;
+}
+
+
+.box_inp{
+  display: flex;
+  width: 684px;
+  padding: 20px;
+  align-items: center;
+  gap: 10px;
+}
+
+.box_text{
+  display: flex;
+  width: 684px;
+  padding: 20px 20px 70px 20px;
+  align-items: center;
+  gap: 10px;
+  resize: none;
+ textarea::placeholder {
+      color:#A7A7A7;
+      font-family: Vollkorn;
+      font-size: 17px;
+      font-style: italic;
+      font-weight: 400;
+      line-height: 22px;
   }
 }
-</script>
+
+.box_btn {
+  display: flex;
+  width: 725px;
+  height: 68px;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  background: var(--foundation-painted-pony-normal-active, #94785D);
+  // &:hover {
+  //   border-radius: 15px;
+  //   font-weight: 700;
+  // }
+}
+.h2{
+  color: var(--foundation-light-white, #FFF);
+  text-align: center;
+
+  /* Headingbold06 */
+  font-family: Encode Sans;
+  font-size: 23px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 120%; /* 27.6px */
+}
+
+</style>
